a52d5c3d2f23eecfe3295450b9cd935e64f20e30
anchor-abhi <theabhishek1802@gmail.com>
2022-01-20 02:09:21 +0530

    login page completed

---
+++ components/footer.js
@@ -0,0 +1,86 @@
+function footer(){
+
+    return `
+    <div id="advertise">
+            <div>
+                <i class="logo fas fa-history"></i>
+                <p> <span class="dark"> NEXT DAY DELIVERY </span><br> For Order Placed by 6pm</p>
+                <!-- <p>For Order Placed by 6pm</p> -->
+            </div>
+            <div>
+                <i class="logo fas fa-truck-moving"></i>
+                <p><span class="dark">FREE DELIVERY</span> <br>  On order above Rs.750</p>
+            </div>
+            <div>   
+                <i class="logo fas fa-percentage"></i>
+                <p><span class="dark">10% OFF</span> <br> On purchase of Rs.2500 & above</p>
+            </div>
+            <div>
+                <i class="logo1 fas fa-cart-plus"></i>
+                <p> <span class="dark1">Cart buster 1 @ Rs.1*</span> <br> For cart value Rs.1000-Rs.1499 </p>
+            </div>
+            <div>
+                <i class="logo1 fas fa-cart-plus"></i>
+                <p><span class="dark1">Cart Buster 2 @ Rs.2*</span> <br>For cart value Rs.1500 & above</p>
+            </div>
+        </div>
+<hr>
+<div id="footer_container">
+<h3>Shop By Brand</h3>
+ <p><strong class="hd">Foods:</strong>Aashirvaad Atta | Aashirvaad Svasti Ghee | Aashirvaad Spices | Aashirvaad Masala | Aashirvaad Iodised Salt | Aashirvaad Organic Dals | Aashirvaad Organic Atta | Aashirvaad Instant Mix | Aashirvaad Instant Meals | Sunfeast Biscuits | Sunfeast YiPPee! Noodles | Sunfeast YiPPee! Pasta | Sunfeast Marie Light | Sunfeast Dark Fantasy | Sunfeast Farmlite | Sunfeast Bounce | Sunfeast Moms Magic | Sunfeast Caker | Sunfeast Wonderz Milk | Fabelle Exquisite Chocolates | Fabelle Choco Deck | Fabelle Soft Centres | Fabelle Cakes & Desserts | B Natural Juice | Jelimals | Candyman Fantastik | Candyman Choco Double Eclairs | Sunbean Gourmet Coffee | Sunbean Beaten Coffee | ITC Master Chef Frozen Snacks | ITC Master Chef Frozen Prawns | ITC Master Chef Cooking Paste | ITC Master Chef All Purpose Gravies | Sunrise Pure | Farmland Frozen Vegetables | Bingo! Chips | Bingo! Mad Angles | Bingo! Original Style | Bingo! Potato Chips | Bingo! Tedhe Medhe | Kitchens Of India Ready To Eat Food
+
+ </p>
+    <p> <strong class="hd">Personal Care:</strong>  Savlon Sanitizer | Savlon Handwash | Savlon Soaps | Savlon Body Wash & Shower Gel | Savlon Antiseptic | Savlon Surface Disinfectant Spray | Savlon Wipes | Savlon Mask | Fiama Handwash | Fiama Soap | Fiama Body Wash & Shower Gel | Engage Perfume | Engage Body Spray | Engage Deo | Essenza Di Wills Perfume | Essenza Di Wills Deo | Essenza Di Wills Body Spray | Essenza Di Wills Body Soaps | Nimwash Vegetable & Fruit Wash | Nimyle Floor Cleaner | Vivel Soaps | Vivel Body Wash & Shower Gel | Shower To Shower Talc | Charmis Cream | Dermafique | Nimeasy Dish Wash Gel</p>
+
+    <p> <strong class="hd">Stationery:</strong> <a href="">Classmate Notebooks</a> | <a href="">Classmate Stationery</a> | <a href="">Paperkraft</a> </p>
+
+    <p><strong class="hd">Matches & Agarbatti</strong> <a href="">Homelites</a> | <a href="">Mangaldeep Agarbatti</a> </p>
+
+ <h3>Shop By Collection</h3>
+   <p><strong class="hd">Food:</strong>Atta | Spices & Masalas | Spices | Masalas | Salt | Organic Dal | Instant Mix Food | Ghee | Ready To Eat Food | Biscuits | Noodles | Pasta | Chocolates | Frozen Snacks | Frozen Prawns | Frozen Vegetables | Chips & Snacks | Beaten Coffee | Gourmet Coffee | Fruit Juice | Milk Shakes | Chocolate Wafers, Candies & Jellies | Frozen Foods</p>
+
+ <p><strong class="hd">Personal care</strong> Handwash | Deo | Perfume | Wet Wipes | Body Spray | Vegetable & Fruit Wash | Floor Cleaner | Soaps | Talc | Body Wash & Shower Gels | Skin Care Products | Antiseptic Liquid | Sanitizer | Disinfectant Spray | Mask</p>
+
+<p> <a href="">Stationery | Matches | Agarbatti</a>
+</p>
+ </div>
+
+<div id="footer_end">
+    <div>
+<h5>INFROMATION</h5>
+<p>
+<a href="">Privacy Policy</a>
+<a href="">Terms of Use</a>
+<a href="">About Us</a>
+</p>
+    </div>
+    <div>
+
+        <h5>LINKS</h5>
+        <p>
+        <a href="">Returns, Refunds and Cancellation</a>
+        <a href="">Billing Terms and Conditions</a>
+        
+        <a href="">Blog</a>
+        </p>
+    </div>
+    <div>
+        <h5>YOUR ACCOUNT</h5>
+        <p>
+            <a href="">Sign in/Register</a>
+        </p>
+    </div>
+    <div>
+        <h5>CONTACT DETAILS</h5>
+        <p>
+            <a href="">Telephone: 1800-120-6682</a>
+            <a href="">suport@ictstore.in</a>
+            <a href="">Contact Us</a>
+        </p>
+    </div>
+</div>
+    
+    `
+}
+
+export default footer;
---
+++ components/footer2.js
@@ -0,0 +1,41 @@
+function footer2(){
+
+    return `
+    <div id="footer_end">
+    <div>
+<h5>INFROMATION</h5>
+<p>
+<a href="">Privacy Policy</a>
+<a href="">Terms of Use</a>
+<a href="">About Us</a>
+</p>
+    </div>
+    <div>
+
+        <h5>LINKS</h5>
+        <p>
+        <a href="">Returns, Refunds and Cancellation</a>
+        <a href="">Billing Terms and Conditions</a>
+        
+        <a href="">Blog</a>
+        </p>
+    </div>
+    <div>
+        <h5>YOUR ACCOUNT</h5>
+        <p>
+            <a href="">Sign in/Register</a>
+        </p>
+    </div>
+    <div>
+        <h5>CONTACT DETAILS</h5>
+        <p>
+            <a href="">Telephone: 1800-120-6682</a>
+            <a href="">suport@ictstore.in</a>
+            <a href="">Contact Us</a>
+        </p>
+    </div>
+</div>
+    `
+}
+
+export default footer2;
