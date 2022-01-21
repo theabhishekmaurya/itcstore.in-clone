09a710dcec8165b1fc89c6ecfece59d5546a3e1f
anchor-abhi <theabhishek1802@gmail.com>
2022-01-22 00:20:02 +0530

    new update, sort features added

---
+++ food.js
@@ -0,0 +1,151 @@
+let food=[
+
+    {
+        image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/40127506-2_a2aa23ab-7eee-4709-a0fe-537e4d69b6c5_275x275.jpg?v=1626185802",
+        name:"Aashirvaad Shudh Chakki Atta 5kg",
+        rating:"49 reviews",
+        price:193
+    },
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/29957_1_786d5248-1465-45cf-9329-aff61630ef56_275x275.jpg?v=1634620788",
+    name:"ITC Master Chef Hara Bhara Kebab 210g",
+    rating:"24 reviews",
+    price:117
+},
+
+{
+image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/29957_1_786d5248-1465-45cf-9329-aff61630ef56_275x275.jpg?v=1634620788",
+name:"Candyman Fantastik Chocobar XL (Pack of 18 Bars)",
+rating:"No reviews",
+price:130
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/flatBOPMPATTA10KgChillioffer3Dpack18-12copy_275x275.jpg?v=1641391059",
+    name:"Aashirvaad Superior MP Atta, 10kg - Free 50g Chilli Powder",
+    rating:"No reviews",
+    price:490
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_Bingo_-Trail-Mix_200g_275x275.jpg?v=1640779332",
+    name:"Bingo Trail Mix",
+    rating:"No reviews",
+    price:375
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_17c06cf5-7d22-4d35-a1ec-81c83d8e7882_275x275.jpg?v=1640778996",
+    name:"Fabelle Heavenly Dark Mousse with Ghana Cocoa",
+    rating:"No reviews",
+    price:225
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_21c21f98-bbde-4336-83fc-48d49715c933_275x275.jpg?v=1640778352",
+    name:"Fabelle Heavenly Almond Mousse with Ghana Cocoa",
+    rating:"No reviews",
+    price:225
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_13e9feda-a253-48a7-b03d-da39c8c3b0da_275x275.jpg?v=1640777746",
+    name:"Fabelle Heavenly Milk Mousse with Ghana Cocoa",
+    rating:"No reviews",
+    price:225
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_206a56de-9d9f-4a17-8d9a-e65147aca656_275x275.jpg?v=1640777290",
+    name:"Fabelle Divine Dark 64% with Colombia Cocoa",
+    rating:"No reviews",
+    price:225
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_729c409e-9b5d-4e5d-b920-c8cbf8e096bb_275x275.jpg?v=1640760711",
+    name:"Fabelle Divine Dark 64% with Ghana Cocoa",
+    rating:"No reviews",
+    price:225
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP_dce66c9e-b676-43de-a5b7-b37ff968c259_275x275.jpg?v=1640758851",
+    name:"Fabelle Divine Dark 84% with Ghana Cocoa",
+    rating:"No reviews",
+    price:225
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/11570632-Fantastik-2.5L-BOP-1000x1000_275x275.jpg?v=1638890246",
+    name:"CANDYMAN FANTASTIK 460G",
+    rating:"No reviews",
+    price:100
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/Hand-rolled-truffles-and-rochers-4x3_1_275X272.jpg?v=1638878204",
+    name:"Hand rolled Truffles & Rochers - 16 pcs",
+    rating:"No reviews",
+    price:1768.82
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/11658246-Bounce-fills-Orange-Carton-FOP-1000x1000_275x275.jpg?v=1637402446",
+    name:"Bounce Fills Orange Vanilla 300g",
+    rating:"No reviews",
+    price:75
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/11658246-Bounce-fills-Strawberry-Carton-FOP-1000x1000_275x275.jpg?v=1637387787",
+    name:"Bounce Fills Strawberry Vanilla 300g",
+    rating:"No reviews",
+    price:75
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/ATTA-10-Kg-NMP-offer-3D-27-10-21-Bop-flat-copy_275x275.jpg?v=1637323662",
+    name:"Aashirvaad Shudh Chakki Atta 10kg + 1 kg Shudh Chakki Atta Free",
+    rating:"No reviews",
+    price:390
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/BOP_2db8c239-c660-4b0c-873e-756bf16a31b4_275x275.jpg?v=1636543548",
+    name:"Fabelle Heart of Gold Collections 10s",
+    rating:"No reviews",
+    price:2100
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/ASATTANNPPRM5KG_8901725117993_2_275x275.jpg?v=1636371883",
+    name:"Select Atta 5 kg + Set of 2 Roti Covers Free Promo",
+    rating:"No reviews",
+    price:315
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/Bingo_ChatarMatarPackaging_BOP_1_275x275.jpg?v=1637061317",
+    name:"Bingo! Tedhe Medhe Chatar Matar",
+    rating:"No reviews",
+    price:10
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP-Primary_DF-Expressions_400g_8901725120184_275x275.jpg?v=1634113167",
+    name:"Dark Fantasy Expressions, 400g",
+    rating:"No reviews",
+    price:200
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/FOP-Primary_Vanilla-Fills_75g_8901725112660_275x275.jpg?v=1634111621",
+    name:"Sunfeast Dark Fantasy Vanilla Fills, 75g",
+    rating:"No reviews",
+    price:20
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/BOPPrimary_DFChocoFills_600g_8901725110833_275x275.jpg?v=1634110323",
+    name:"Sunfeast Dark Fantasy Choco Fills, 600g | Rs.280/-",
+    rating:"No reviews",
+    price:224
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/DF-CHNUTDIPPED100G_8901725116828_2_275x275.jpg?v=1640862809",
+    name:"Sunfeasy Dark Fantasy Desserts Choco nut dipped, 100g | Rs.50/-",
+    rating:"No reviews",
+    price:50
+},
+{
+    image:"https://cdn.shopify.com/s/files/1/0173/7644/4470/products/The-Deluxe-Selection-by-Nutmeg--The-Gourmet-Shop-Image-2_275x275.jpg?v=1639140126",
+    name:"The Deluxe Selection",
+    rating:"No reviews",
+    price:7668.82
+},
+
+
+
+]
